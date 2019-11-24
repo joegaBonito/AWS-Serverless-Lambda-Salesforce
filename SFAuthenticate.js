ffe8aff421f0a63f4828ffaeb7ff2d1f28e6ce54
joegabonito <joejung5678@gmail.com>
2019-11-24 15:37:52 -0500

    Initial Commit

---
+++ SFAuthenticate.js
@@ -0,0 +1,46 @@
+console.log('Loading function');
+var jsforce = require('jsforce');
+
+
+exports.handler = (event, context) => {
+
+      var conn = new jsforce.Connection({
+        // you can change loginUrl to connect to sandbox or prerelease env.
+        loginUrl : 'https://test.salesforce.com'
+      });
+
+      conn.login(process.env.username, process.env.password, function(err, userInfo) {
+        if (err) { return console.error(err); }
+        // Now you can get the access token and instance URL information.
+        // Save them to establish connection next time.
+        console.log(conn.accessToken);
+        console.log(conn.instanceUrl);
+
+        // logged in user property
+        console.log("User ID: " + userInfo.id);
+        console.log("Org ID: " + userInfo.organizationId);
+      });
+
+      console.log('process.env.instanceUrl ===========> ' + process.env.instanceUrl);
+      console.log('process.env.accessToken ===========> ' + process.env.accessToken);
+
+      conn = new jsforce.Connection({
+        instanceUrl : process.env.instanceUrl, //instance URL
+        accessToken : process.env.accessToken //access token
+      });
+
+      var body = JSON.parse(process.env.requestBody);
+
+      var item = conn.apex.post(process.env.endpoint, body, function(err, res) {
+        if (err) { return console.error(err); }
+        console.log("response: ", res);
+        // the response object structure depends on the definition of apex class
+        return res;
+      });
+    
+    return {
+        'statusCode': 200,
+        'headers': { 'Content-Type': 'application/json' },
+        'body': JSON.stringify(item)
+    }
+};
