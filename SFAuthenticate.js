0574d35f273bf56a9daf7599ffe0e7b68a3058d1
Joe Jung <JJung@retrievermed.com>
2019-11-25 17:36:57 -0500

    RequestBody Tested Version

--- SFAuthenticate.js
+++ SFAuthenticate.js
@@ -2,45 +2,45 @@ console.log('Loading function');
 var jsforce = require('jsforce');
 
 
-exports.handler = (event, context) => {
+exports.handler =  (event, context,callback) => {
 
       var conn = new jsforce.Connection({
         // you can change loginUrl to connect to sandbox or prerelease env.
         loginUrl : 'https://test.salesforce.com'
       });
-
+      
       conn.login(process.env.username, process.env.password, function(err, userInfo) {
         if (err) { return console.error(err); }
         // Now you can get the access token and instance URL information.
         // Save them to establish connection next time.
         console.log(conn.accessToken);
         console.log(conn.instanceUrl);
-
+      
         // logged in user property
         console.log("User ID: " + userInfo.id);
         console.log("Org ID: " + userInfo.organizationId);
       });
-
+      
       console.log('process.env.instanceUrl ===========> ' + process.env.instanceUrl);
       console.log('process.env.accessToken ===========> ' + process.env.accessToken);
-
+      
       conn = new jsforce.Connection({
         instanceUrl : process.env.instanceUrl, //instance URL
         accessToken : process.env.accessToken //access token
       });
 
-      var body = JSON.parse(process.env.requestBody);
+      // var body = JSON.parse(process.env.requestBody);
+      var body = event.body; //body taken in by a Request Body.
 
-      var item = conn.apex.post(process.env.endpoint, body, function(err, res) {
-        if (err) { return console.error(err); }
+      var item = {};
+      
+      conn.apex.post(process.env.endpoint, body, function(err, res) {
+        if (err) {  
+          callback(err, null); 
+          return console.log('errr' + err); 
+        }
         console.log("response: ", res);
         // the response object structure depends on the definition of apex class
-        return res;
+        callback(null, res);
       });
-    
-    return {
-        'statusCode': 200,
-        'headers': { 'Content-Type': 'application/json' },
-        'body': JSON.stringify(item)
-    }
 };
