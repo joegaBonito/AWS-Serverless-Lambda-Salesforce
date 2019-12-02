a217dd4e76e5de6a85a8cf948546d8b582c335af
Joe Jung <JJung@retrievermed.com>
2019-12-02 14:35:41 -0500

    Promise has been added to use accessToken, refreshToken, and instanceUrl dynamically.

--- SFAuthenticate.js
+++ SFAuthenticate.js
@@ -1,73 +1,98 @@
 console.log('Loading function');
 var jsforce = require('jsforce');
+var conn;
+
+exports.handler = (event, context,callback) => {
+  /**
+   * Promise function that handles login and passes the connection info as a parameter.
+   */
+  var loginPromise = new Promise(function(resolve,reject) {
+            conn = new jsforce.Connection({
+              // you can change loginUrl to connect to sandbox or prerelease env.
+              loginUrl : 'https://test.salesforce.com'
+            });
+            conn.login(process.env.username, process.env.password, function(err, userInfo) {
+              if (err) {
+              reject(Error(err));
+              } else {
+                // Now you can get the access token and instance URL information.
+                // Save them to establish connection next time.
+                console.log('Access Token: '+conn.accessToken);
+                console.log('Refresh Token: '+conn.refreshToken);
+                console.log('Instance URL: '+conn.instanceUrl);
+
+                // // logged in user property
+                console.log("User ID: " + userInfo.id);
+                console.log("Org ID: " + userInfo.organizationId);
+                resolve(conn);
+              }
+            });
+    });
+  
+
+  /**
+   * Waits for the promise to be completed then handles the logic afterwards.
+   */
+  loginPromise.then(function(promisedConn) {
+    console.log('promisedConn Access Token: '+promisedConn.accessToken);
+    console.log('promisedConn Refresh Token: '+promisedConn.refreshToken);
+    console.log('promisedConn Instance URL: '+promisedConn.instanceUrl);
+    conn = new jsforce.Connection({
+      oauth2 : {
+        clientId : process.env.consumerKey, //'<your Salesforce OAuth2 client ID is here>',
+        clientSecret : process.env.consumerSecret, //'<your Salesforce OAuth2 client secret is here>',
+        redirectUri : process.env.redirectUri //'<your Salesforce OAuth2 redirect URI is here>'
+      },
+        instanceUrl : promisedConn.instanceUrl, //'<your Salesforce server URL (e.g. https://na1.salesforce.com) is here>'
+        accessToken : promisedConn.accessToken, //'<your Salesforrce OAuth2 access token is here>'
+        refreshToken : promisedConn.refreshToken //'<your Salesforce OAuth2 refresh token is here>'
+    });
+
+    conn.on("refresh", function(accessToken, res) {
+      console.log('On refresh access token: '+accessToken);
+      console.log('On refresh: '+res);
+      // Refresh event will be fired when renewed access token
+      // to store it in your storage for next request
+    });
 
 
-exports.handler =  (event, context,callback) => {
+  console.log('event ======>' + event);
+  var body;
+  if(typeof event ==  "string") {
+    body = JSON.parse(event); //When requestBody is in string format so it needs to be parsed into JSON.
+  } else {
+    console.log('json format stringified event =====> ' + JSON.stringify(event));
+    console.log('json format event =====> ' +JSON.parse(JSON.stringify(event)));
+    var eventTemp = JSON.parse(JSON.stringify(event));
+    body = eventTemp.body; //When requestBody is in JSON 
+  }
 
-      var conn = new jsforce.Connection({
-        // you can change loginUrl to connect to sandbox or prerelease env.
-        loginUrl : 'https://test.salesforce.com'
-      });
-      
-      conn.login(process.env.username, process.env.password, function(err, userInfo) {
-        if (err) { return console.error(err); }
-        // Now you can get the access token and instance URL information.
-        // Save them to establish connection next time.
-        console.log(conn.accessToken);
-        console.log(conn.instanceUrl);
-      
-        // logged in user property
-        console.log("User ID: " + userInfo.id);
-        console.log("Org ID: " + userInfo.organizationId);
-      });
-      
-      console.log('process.env.instanceUrl ===========> ' + process.env.instanceUrl);
-      console.log('process.env.accessToken ===========> ' + process.env.accessToken);
-      
-      conn = new jsforce.Connection({
-        instanceUrl : process.env.instanceUrl, //instance URL
-        accessToken : process.env.accessToken //access token
-      });
-      
-      console.log('event ======>' + event);
-      console.log('event.body =====>' + event.body);
-      console.log('event.data =====>' + event.data);
-      // var body = JSON.parse(process.env.requestBody);
-      var body;
-      if(typeof event ==  "string") {
-        body = JSON.parse(event); //When requestBody is in string format so it needs to be parsed into JSON.
-      } else {
-        console.log('json format stringified event =====> ' + JSON.stringify(event));
-        console.log('json format event =====> ' +JSON.parse(JSON.stringify(event)));
-        var eventTemp = JSON.parse(JSON.stringify(event));
-        body = eventTemp.body; //When requestBody is in JSON 
-      }
-      
-      console.log('body ===> ' + body);
-      console.log('typeof body ===> ' + typeof body);
-      if(typeof body == "string") {
-        
-        body = JSON.parse(body);
-        console.log('If body is string is called, and the body is pared like ===> ' + body);
-      }
+  console.log('body ===> ' + body);
+  console.log('typeof body ===> ' + typeof body);
+  if(typeof body == "string") {
+    body = JSON.parse(body);
+    console.log('If body is string is called, and the body is pared like ===> ' + body);
+  }
 
-      conn.apex.post(process.env.endpoint, body, function(err, res) {
-        if (err) {  
-          callback(err);
-        }
-        console.log("response: ", res);
-        const response = {
-          "isBase64Encoded": false,
-          "statusCode": 200,
-          "headers": {
-            "Access-Control-Allow-Origin" : "*",
-            "Access-Control-Allow-Credentials" : true,
-            "Access-Control-Allow-Methods": "PUT,DELETE,POST,GET,Head,OPTIONS",
-            "Access-Control-Allow-Headers":"*"
-          },
-          "body": JSON.stringify(res)
-        };
-        // the response object structure depends on the definition of apex class
-        callback(null, response);
-      });
+  conn.apex.post(process.env.endpoint, body, function(err, res) {
+    if (err) {  
+      callback(err);
+    }
+    console.log("response: ", res);
+    const response = {
+      "isBase64Encoded": false,
+      "statusCode": 200,
+      "headers": {
+        "Access-Control-Allow-Origin" : "*",
+        "Access-Control-Allow-Credentials" : true,
+        "Access-Control-Allow-Methods": "PUT,DELETE,POST,GET,Head,OPTIONS",
+        "Access-Control-Allow-Headers":"*"
+      },
+      "body": JSON.stringify(res)
+    };
+    callback(null,response);
+  });
+  }).catch(function(e) {
+    callback(e);
+  });
 };
